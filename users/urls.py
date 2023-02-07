7615f58be19b849b9e51576c497cc6f059d7eb8f
artem22822 <35076622+artem22822@users.noreply.github.com>
2023-02-07 14:46:51 +0200

    django
    django

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path(r'', views.singup, name='singup'),
+    path('login/', views.login_page, name='login'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout_page, name='logout'),
+]
---
+++ users/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='login')
+def home(reauest):
+    return render(reauest,'accounts/home.html')
+
+def singup(reauest):
+    if reauest.method == 'POST':
+        uname = reauest.POST.get('username')
+        email = reauest.POST.get('email')
+        password1 = reauest.POST.get('password1')
+        password2 = reauest.POST.get('password2')
+
+        if password1 != password2:
+            return HttpResponse('You Password mismatch !!!')
+        else:
+            my_user = User.objects.create_user(uname,email,password1)
+            my_user.save()
+            print(uname, email, password1, password2)
+
+            return redirect('login')
+
+    return render(reauest, 'accounts/singup.html')
+
+def login_page(reauest):
+    if reauest.method == 'POST':
+        username = reauest.POST.get('username')
+        password = reauest.POST.get('pass')
+        user = authenticate(reauest, username = username, password = password)
+        if user is not None:
+            login(reauest, user)
+            return redirect('home')
+        else:
+            return HttpResponse('Username or Password is incorrect!!!!!')
+    return  render(reauest, 'accounts/login.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
