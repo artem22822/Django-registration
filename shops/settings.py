9906ec2fdd076810b1966d85b14807e5db8a899f
artem22822 <35076622+artem22822@users.noreply.github.com>
2023-02-09 20:27:36 +0200

    setings.py update1

--- shops/settings.py
+++ shops/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-27!!u9txu4#q2i26pkela3#6a$7dn4kt_53ra^$*)pi^dr2r61'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-registration-production.up.railway.app']
 
