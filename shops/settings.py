c0b4f149c21199bb1e6b564612f5cfd6a6bcb4d2
artem22822 <35076622+artem22822@users.noreply.github.com>
2023-02-14 21:39:22 +0200

    land1

--- shops/settings.py
+++ shops/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-27!!u9txu4#q2i26pkela3#6a$7dn4kt_53ra^$*)pi^dr2r61'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['127.0.0.1', 'django-registration-production.up.railway.app']
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
