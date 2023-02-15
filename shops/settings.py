b0989bbaca7b76d0fa057001de200e4232bc26d9
artem22822 <35076622+artem22822@users.noreply.github.com>
2023-02-15 13:45:51 +0200

    12

--- shops/settings.py
+++ shops/settings.py
@@ -122,9 +122,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles/"),
-]
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, "staticf"),
+#]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
